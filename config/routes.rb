d4afc88ce08bf989b5cedd5ab7c9b8647fac3c9b
thomas <tomhaury@gmail.com>
2015-10-15 13:27:16 -0700

    checkpoint 26 assingment completed

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,10 @@ Rails.application.routes.draw do
 
    get 'welcome/about'
 
+   get 'welcome/contact'
+
+   get "welcome/faq"
+
    root to: 'welcome#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
