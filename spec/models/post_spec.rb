3533a5a964958b8d504311aa18527f266edd96bd
thomas <tomhaury@gmail.com>
2016-02-10 18:06:25 -0800

    checkpoint35 completed

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -7,7 +7,15 @@ RSpec.describe Post, type: :model do
 # #4
  let(:post) { topic.posts.create!(title: RandomData.random_sentence, body: RandomData.random_paragraph) }
 
- it { should belong_to(:topic) }
+  it { should belong_to(:topic) }
+
+ # #1
+  it { is_expected.to validate_presence_of(:title) }
+  it { is_expected.to validate_presence_of(:body) }
+  it { is_expected.to validate_presence_of(:topic) }
+# 2
+  it { is_expected.to validate_length_of(:title).is_at_least(5) }
+  it { is_expected.to validate_length_of(:body).is_at_least(20) }
 
    describe "attributes" do
  # #2
