f0f9d2e11d889fbf446ea39760b69a32ba3d2837
thomas <tomhaury@gmail.com>
2016-05-20 12:07:34 -0700

    cp44a done

--- app/mailers/favorite_mailer.rb
+++ app/mailers/favorite_mailer.rb
@@ -15,4 +15,12 @@ class FavoriteMailer < ApplicationMailer
  # #19
      mail(to: user.email, subject: "New comment on #{post.title}")
    end
+   def new_post(post)
+     headers["Message-ID"] = "<comments/#{comment.id}@your-app-name.example>"
+     headers["In-Reply-To"] = "<post/#{post.id}@your-app-name.example>"
+     headers["References"] = "<post/#{post.id}@your-app-name.example>"
+
+     @post = post
+
+     mail(to: post.usr.email, subject: "Your following, #{post.title}")
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -4,6 +4,7 @@ class Post < ActiveRecord::Base
   has_many :comments, dependent: :destroy
   has_many :votes, dependent: :destroy
   has_many :favorites, dependent: :destroy
+  after_create :create_favorite
   has_many :labelings, as: :labelable
   has_many :labels, through: :labelings
   has_many :commentings, as: :commentable
@@ -39,6 +40,10 @@ class Post < ActiveRecord::Base
     new_rank = points + age_in_days
     update_attribute(:rank, new_rank)
   end
+      def create_favorite
+        Favorite.create(post: self,user: self.user)
+        FavoriteMailer.new_post(self).deliver_now
+      end
   private
 
     def create_vote
